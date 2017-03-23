5fdaf26fbbe0e957ef013e3d4d86eea66bd00110
HiFi-LoFi <no-mail@HiFi-LoFi.de>
2017-03-23 23:09:35 +0100

    License stuff

--- Squeeze.h
+++ Squeeze.h
@@ -1,5 +1,5 @@
 // ==================================================================================
-// Copyright (c) 2016 HiFi-LoFi
+// Copyright (c) 2017 HiFi-LoFi
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
